6bb5b389f2d3102c5e3e04593c700075fcb5b864
klinelozada <klinelozada@gmail.com>
2015-02-16 10:44:17 +0800

    Created Registration and Login Page

--- wp-content/themes/raceit/page-create-an-account.php
+++ wp-content/themes/raceit/page-create-an-account.php
@@ -1,4 +1,62 @@
-<?php get_header(); ?>
+<?php
+/*
+Template Name: Mavericks Create Account
+*/
+get_header(); 
+?>
+<style>
+.col-md-12 {
+	background:none repeat scroll 0 0 #CCCCCC;
+	}
+#mav-left {
+	width:388px;
+	height:auto;
+	float:left;
+	background:#fff;
+	padding:10px;
+}
+#mav-holder {
+	max-width:951px;
+	margin:0 auto;
+}
+#mav-right {
+	background:url('http://localhost/raceit/wp-content/themes/raceit/images/admin-img/createaccount-bgd.jpg') no-repeat scroll right top #EEEEEE;
+	width:563px;
+	height:819px;
+	float:right;
+}
+#mav-createfont {
+	font-family: Raleway,Helvetica,Arial,sans-serif;
+	font-weight: 600;
+	line-height: 1.1em;
+	font-size: 1.6em;
+	color: #719629 !important;
+	margin: 20px 0 9px !important;
+	padding: 0;
+}
+#mav-createfont-2 {
+	font-family: Raleway,Helvetica,Arial,sans-serif;
+	font-weight: 600;
+	line-height: 1.1em;
+	font-size: 1.6em;
+	margin: 20px 0 9px !important;
+	padding: 0;
+	position:relative;
+	top: 8px;
+	text-indent:15px;
+}
+#mav-fieldsreq {
+	font-family:Raleway,Helvetica,Arial,sans-serif;
+	font-size:.9em;
+	font-weight:400;
+	line-height:1.5em;
+	padding:0;
+	color:#666;
+}
+#wppb-register-user ul li label {
+	display:none;
+}
+</style>
 <div class="main-content post">
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	<div class="row">
