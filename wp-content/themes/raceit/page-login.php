fefa78dc21a900f28b538f117f3e886957e38375
Maron Aquillo <maron12mrn13@gmail.com>
2015-02-18 17:40:23 +0800

    Modified Login form

--- wp-content/themes/raceit/page-login.php
+++ wp-content/themes/raceit/page-login.php
@@ -8,7 +8,8 @@ get_header();
 	<div class="row">	
 		<div class="col-md-offset-2 col-md-8">
 			<div class="row">
-				<div class="col-md-7">
+				<div class="col-md-7 wrap">
+					<h1>Login to your account</h1>
 					<?php $args = array(
 					        'echo'           => true,
 					        'redirect'       => site_url('/events'), 
@@ -27,9 +28,27 @@ get_header();
 					); ?> 
 
 					<?php wp_login_form( $args ); ?> 
-					<a href="<?php echo wp_lostpassword_url(); ?>" title="Lost Password">Forgot Password</a>
+					<a href="<?php echo wp_lostpassword_url(); ?>" class="lost-password" title="Lost Password">Forgot Password</a>
 				</div>
-				<div class="col-md-5"></div>
+				<div class="col-md-5 accountlogin">
+					<div class="accountlogin-details">
+						<h2>Don't have a RaceIt account?</h2>
+						<a href="#" class="create-account">Create Account</a>
+							
+						<div class="socialoptions">
+							<p>Use your Facebook or Google account to create/login</p>
+						
+							<div id="sociallogin" style="position: relative;">
+							    <div class="newSection padTop">
+							        <p class="legalText">We don't post anything without your permission.</p>
+								    <p class="legalText">
+								        By clicking "Facebook" or "Google+" you confirm that you accept the <a href="http://www.raceit.com/legal.aspx">Terms of Service</a> and <a href="http://www.raceit.com/legal/privacy-policy.aspx">Privacy Policy</a>.
+							        </p>
+							    </div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
